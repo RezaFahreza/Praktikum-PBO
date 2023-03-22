84ab2b3af092de123a2e13247aedfb89bc387c41
Rizky A. Fahreza <rizkyfareza1@gmail.com>
2023-03-22 22:49:56 +0700

    Update Segitiga.java

--- Praktikum 4/org/bangundatar/Segitiga.java
+++ Praktikum 4/org/bangundatar/Segitiga.java
@@ -1,3 +1,7 @@
+// Segitiga.java 22/03/22
+// Penulis : Rizky Akhmad Fahreza
+// Deskripsi : Kelas Segitiga dengan Konstruktor dan Methodnya
+
 package org.bangundatar;
 
 import org.poligon.Poligon;
